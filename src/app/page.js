c7a91e3b0a861586b61a29c50e9c9873d5589e9c
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-27 21:08:41 +0200

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+'use client'
+
+import About from "@/components/About";
+import Experience from "@/components/Experience";
+import Hero from "@/components/Hero";
+
+
+export default function Home() {
+  return (
+  <div>
+    <Hero/>
+    <About/>
+    <Experience/>
+    </div>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,116 @@
+"use client";
+
+import Image from "next/image";
+import { heroIcons } from "@/assests";
+import {
+  useMotionValue,
+  useTransform,
+  motion,
+  useSpring,
+  delay,
+} from "framer-motion";
+import { useState } from "react";
+
+const Hero = () => {
+  const [windowOffset, setWindowOffset] = useState({
+    innerWidth: 0,
+    innerHeight: 0,
+  });
+  const [mouseMove, setMouseMove] = useState(false);
+  const [buttonHover, setButtonHover] = useState(false);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    x.set(clientX); // we use set instead of useState its change value directly wihtout rerender
+    y.set(clientY);
+    // console.log(clientX,clientY,x,y)
+  };
+
+  const handleMouseEnter = () => {
+    setWindowOffset({
+      innerWidth: window.innerWidth,
+      innerHeight: window.innerHeight,
+    });
+    setMouseMove(true);
+    // console.log(innerWidth,innerHeight)
+  };
+
+  const { innerWidth, innerHeight } = windowOffset;
+
+  const xSpring = useSpring(x, { stiffness: 100, damping: 10 });
+  const ySpring = useSpring(y, { stiffness: 100, damping: 10 });
+
+  const rotateY = useTransform(xSpring, [0, innerWidth], [-30, 30]);
+  const rotateX = useTransform(ySpring, [0, innerHeight], [10, -50]);
+
+  return (
+    <div
+      className="h-screen grid place-items-center"
+      onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+    >
+      <div>
+        <div className="flex flex-col items-center justify-center gap-y-3 font-light capitalize">
+          <motion.div
+            className="flex items-center justify-center"
+            style={{
+              rotateX: mouseMove ? rotateX : 0,
+              rotateY: mouseMove ? rotateY : 0,
+              transition: "0.1s",
+            }}
+          >
+            <Image
+              src={"/person.png"}
+              width={400}
+              height={400}
+              alt="Person Image"
+              priority={true}
+              className="h-auto w-[150px]"
+            />
+            <motion.span
+              className="absolute text-3xl font-semibold text-white"
+              initial={{ scale: 0 }}
+              animate={{
+                opacity: buttonHover ? 0 : 1,
+                scale: buttonHover ? 2 : 0,
+                y: buttonHover ? -40 : 0,
+              }}
+              transition={{ opacity: { delay: 0.4 } }}
+            >
+              Hi
+            </motion.span>
+          </motion.div>
+          <h1 className="text-center text-3xl font-bold tracking-wider text-gray-500 sm:text-2xl">
+            My Name is Ismail Bakhach &
+          </h1>
+          <p className="text-lg tracking-wider text-gray-700">
+            I like animations 😍
+          </p>
+        </div>
+        <div className="mt-8 flex justify-center gap-x-10 text-3xl text-yellow-600 sm:text-2xl">
+          {heroIcons.map((icon, i) => (
+            <a
+              href="#"
+              key={i}
+              className="rounded-lg hover:bg-red-400 hover:text-white transition-colors "
+            >
+              {icon}
+            </a>
+          ))}
+        </div>
+        <a
+          href="#"
+          className=" mx-auto text-white hover:bg-red-500 transition-colors mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider"
+          onMouseEnter={() => setButtonHover(true)}
+          onMouseLeave={() => setButtonHover(false)}
+        >
+          Talk to me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
